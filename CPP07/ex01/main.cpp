b9736766c0819501b0d4440a4555c1ff0b0a94fa
Manon Prigent <manonprigent@MacBook-Pro-de-Manon-2.local>
2022-06-04 17:20:31 +0200

    main de correction

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,11 +6,32 @@
 /*   By: mprigent <mprigent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:55:59 by mprigent          #+#    #+#             */
-/*   Updated: 2022/06/04 15:19:11 by mprigent         ###   ########.fr       */
+/*   Updated: 2022/06/04 17:20:05 by mprigent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+
+// class Awesome
+// {
+// public:
+// Awesome( void ) : _n( 42 ) { return; }
+// int get( void ) const { return this->_n; }
+// private:
+// int _n;
+// };
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+// template< typename T >
+// void print( T const & x ) { std::cout << x << std::endl; return; }
+// int main() {
+// int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+// Awesome tab2[5];
+
+// iter( tab, 5, print );
+// iter( tab2, 5, print );
+
+// return 0;
+// }
 
 int main(void)
 {
