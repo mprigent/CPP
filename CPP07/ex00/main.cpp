26c6c7b66f529348a8360ba657c6acf4195dc3cf
Manon Prigent <manonprigent@MacBook-Pro-de-Manon-2.local>
2022-06-04 17:23:31 +0200

    correction main ex00

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,12 +6,41 @@
 /*   By: mprigent <mprigent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:01:24 by mprigent          #+#    #+#             */
-/*   Updated: 2022/06/01 14:55:35 by mprigent         ###   ########.fr       */
+/*   Updated: 2022/06/04 17:23:09 by mprigent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 #include <iostream>
+
+// class Awesome
+// {
+// public:
+// Awesome(void) : _n(0) {}
+// Awesome( int n ) : _n( n ) {}
+// Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+// bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+// bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+// bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+// bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+// bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+// bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+// int get_n() const { return _n; }
+// private:
+// int _n;
+// };
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+// int main(void)
+// {
+// Awesome a(2), b(4);
+
+// swap(a, b);
+// std::cout << a << " " << b << std::endl;
+// std::cout << max(a, b) << std::endl;
+// std::cout << min(a, b) << std::endl;
+// return (0);
+// }
 
 int main(void)
 {
